b2df1c0ff8dc627f52cfb067605e8d15bddc515a
Yousra Goraâni <s1113909@student.windesheim.nl>
2021-11-29 17:29:10 +0100

    Add project files.

---
+++ AfvalWebApi/Models/BaseContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AfvalWebApi.Models
+{
+    public class BaseContext : DbContext
+    {
+        public BaseContext(DbContextOptions<BaseContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Report> Reports { get; set; }
+    }
+}
---
+++ AfvalWebApi/Models/Report.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AfvalWebApi.Models
+{
+    public class Report
+    {
+        public int Id { get; set; }
+        public string Image { get; set; }
+        public string Location { get; set; }
+        public string Problem { get; set; }
+    }
+}
